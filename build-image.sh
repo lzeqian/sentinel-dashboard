9a2cc306cccb4eec54bec69e31ec980bd51f8b0f
liaomin@gvt861.com <liaomin@gvt861.com>
2020-11-13 16:08:50 +0800

    linux 字符集问题

--- build-image.sh
+++ build-image.sh
@@ -8,4 +8,5 @@ do
     DASHBOARD_SERVER_VERSION=`echo "${file%.*}"  | awk '{s=split($1,arr,"-");print(arr[length(arr)])}'`
   fi
 done <<< "$(ls -l ./bin | awk '{print($9)}')"
+
 docker build -t jiaozi/sentinel-dashboard:${DASHBOARD_SERVER_VERSION} --build-arg DASHBOARD_SERVER_VERSION=${DASHBOARD_SERVER_VERSION} -f ./Dockerfile .
